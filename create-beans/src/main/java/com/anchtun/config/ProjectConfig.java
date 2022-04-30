0d9a51438cb9f0cfa2768e50630db26b14d74e33
Mohammed Ayman <m.ayman@tamkeentech.sa>
2022-04-30 11:57:41 +0300

    first commit

---
+++ create-beans/src/main/java/com/anchtun/config/ProjectConfig.java
@@ -0,0 +1,43 @@
+package com.anchtun.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.anchtun.beans.Product;
+
+/*
+ * Spring @Configuration annotation is part of the spring core framework.
+ * Spring Configuration annotation indicates that the class has @Bean definition methods.
+ * So Spring container can process the class and generate Spring Beans to be used in the application.
+ */
+@Configuration
+public class ProjectConfig {
+
+	/**
+	 * 
+	 * @Bean which lets spring know that it needs to call this method when it
+	 *       initializes its context and adds the returned value to the context
+	 */
+	@Bean
+	Product product() {
+		var prod = new Product();
+		prod.setName("Laptop Sony vaio");
+		return prod;
+	}
+
+	/**
+	 * the method names usually follow verbs notation. But for the methods which we will use to create beans,
+	 * we can use nouns as names. This will be a good practice as the method names will become bean names as
+	 * well in the context.
+	 */
+	@Bean
+	String helloWorld() {
+		return "Hello world";
+	}
+
+	@Bean
+	Integer favoriteNumber() {
+		return 5;
+	}
+
+}
---
+++ create-beans/src/main/java/com/anchtun/main/Test.java
@@ -0,0 +1,45 @@
+package com.anchtun.main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.anchtun.beans.Product;
+import com.anchtun.config.ProjectConfig;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		/**
+		 * this object is NOT a bean
+		 */
+		Product productNonSC = new Product();
+		productNonSC.setName("Iphone 12");
+		System.out.println("Product from non-spring context is: " + productNonSC.getName());
+
+		/**
+		 * the var keyword was introduced in Java 10. Type inference is used in var
+		 * keyword in which it detects automatically the datatype of a variable based on
+		 * the surrounding context.
+		 */
+		var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+		/**
+		 * this object is a bean: we get it from the context (memory inside spring
+		 * framework)
+		 */
+		Product productSC = context.getBean(Product.class);
+		System.out.println("Product from spring context is: " + productSC.getName());
+
+		/**
+		 * we don't need to use any explicit casting while fetching a bean from context.
+		 * Spring is smart enough to look of the bean of the type you requested in its
+		 * context. if such a bean doesn't exist, Spring will throw an exception.
+		 */
+		String hello = context.getBean(String.class);
+		System.out.println("String value from spring context is: " + hello);
+
+		Integer number = context.getBean(Integer.class);
+		System.out.println("Integer value from spring context is: " + number);
+
+	}
+}
