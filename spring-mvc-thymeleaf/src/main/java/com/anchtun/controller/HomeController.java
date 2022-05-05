d6504d8263c8bc15370dcc45ef6ccb839059be33
mayman <m.ayman@tamkeentech.sa>
2022-05-05 17:27:33 +0300

    first thymeleaf example/ Model interface

--- spring-mvc-thymeleaf/src/main/java/com/anchtun/controller/HomeController.java
+++ spring-mvc-thymeleaf/src/main/java/com/anchtun/controller/HomeController.java
@@ -1,13 +1,17 @@
 package com.anchtun.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping(value = { "", "/", "home" })
-	public String homePage() {
+	// Model in an interface inside spring mvc which will act as an container
+	// between UI and backend code
+	public String homePage(Model model) {
+		model.addAttribute("userName", "Anchtun");
 		return "home.html";
 	}
 
