c40aac4e6b294507df1379361985cd6c98a0b456
mayman <m.ayman@tamkeentech.sa>
2022-05-06 00:09:39 +0300

    submit data using @RequestParam

--- spring-mvc-thymeleaf/src/main/java/com/anchtun/config/WebConfig.java
+++ spring-mvc-thymeleaf/src/main/java/com/anchtun/config/WebConfig.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/aboutme").setViewName("aboutme");
 	}
--- spring-mvc-thymeleaf/src/main/java/com/anchtun/controller/ContactController.java
+++ spring-mvc-thymeleaf/src/main/java/com/anchtun/controller/ContactController.java
@@ -1,15 +1,30 @@
 package com.anchtun.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ContactController {
+
+	Logger log = LoggerFactory.getLogger(ContactController.class);
 
 	@RequestMapping("/contact")
 	public String homePage(Model model) {
 		return "contact.html";
 	}
 
+	@RequestMapping(value = "/saveMsg", method = RequestMethod.POST)
+	public ModelAndView saveMsg(@RequestParam String fullName, @RequestParam String mobile,
+			@RequestParam String subject) {
+		log.info("fullName=" + fullName);
+		log.info("mobile=" + mobile);
+		log.info("subject=" + subject);
+		return new ModelAndView("redirect:/contact");
+	}
 }
