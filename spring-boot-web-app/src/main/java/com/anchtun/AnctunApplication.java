9dbcd2a6d5c81cfd1850d686081d643fa39158ed
mayman <m.ayman@tamkeentech.sa>
2022-05-05 14:05:07 +0300

    use debug mode and exclude some auto configuration

--- spring-boot-web-app/src/main/java/com/anchtun/AnctunApplication.java
+++ spring-boot-web-app/src/main/java/com/anchtun/AnctunApplication.java
@@ -2,8 +2,11 @@ package com.anchtun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// exclude DataSourceAutoConfiguration and I don't want that the auto-configuration will be created
+// and also bean should not be created by spring boot when the server start up
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class AnctunApplication {
 
 	public static void main(String[] args) {
