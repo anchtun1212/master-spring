39a46e1f831a30d1c942bd71f5a8821000b09285
mayman <m.ayman@tamkeentech.sa>
2022-05-21 22:33:20 +0300

    Using NamedParameterJdbcTemplate

--- spring-boot-h2-database-spring-jdbc/src/main/java/com/anchtun/service/ContactService.java
+++ spring-boot-h2-database-spring-jdbc/src/main/java/com/anchtun/service/ContactService.java
@@ -5,19 +5,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.annotation.ApplicationScope;
 
 import com.anchtun.constants.Constants;
 import com.anchtun.model.Contact;
 import com.anchtun.repository.ContactRepository;
 
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
-@ApplicationScope
-@Data
 public class ContactService {
 
 	@Autowired
