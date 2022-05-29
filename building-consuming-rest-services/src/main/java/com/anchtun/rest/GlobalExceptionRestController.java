0177ff9cd94a8df1cd0f14e5c9966d92873db1e8
mayman <m.ayman@tamkeentech.sa>
2022-05-29 21:33:30 +0300

    Use @JsonIgnore and @JsonIgnoreProperties.

--- building-consuming-rest-services/src/main/java/com/anchtun/rest/GlobalExceptionRestController.java
+++ building-consuming-rest-services/src/main/java/com/anchtun/rest/GlobalExceptionRestController.java
@@ -1,6 +1,5 @@
 package com.anchtun.rest;
 
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
